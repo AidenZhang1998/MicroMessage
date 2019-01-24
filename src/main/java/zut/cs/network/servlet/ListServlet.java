bd7ad65160099f703c41a82cfd88c3d39b2edadf
LetMe666 <201608030215@zut.edu.cn>
2019-01-24 18:10:31 +0800

    Complete data display and query in the background management page

--- src/main/java/zut/cs/network/servlet/ListServlet.java
+++ src/main/java/zut/cs/network/servlet/ListServlet.java
@@ -1,15 +1,22 @@
 package zut.cs.network.servlet;
-
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Driver;
+
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import zut.cs.network.bean.Message;
 
 /*
  * 列表初始化控制
@@ -21,8 +28,38 @@ public class ListServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		try {
+		    req.setCharacterEncoding("UTF-8");
+			String command= req.getParameter("command");
+			String description =req.getParameter("description");
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://114.116.163.105:3306/micro_message","root","Aa184550!");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://114.116.163.105:3306/micro_message?useUnicode=true&characterEncoding=utf-8","root","Aa184550!");
+			StringBuilder sql=new StringBuilder("select ID,COMMAND,DESCRIPTION,CONTENT FROM MESSAGE where 1=1");
+			List<String> paramList = new ArrayList<String>();
+			if (command!=null && !"".equals(command.trim())){
+                sql.append(" and COMMAND=?");
+                paramList.add(command);
+            }
+            if (description != null && !"".equals(description.trim())){
+                sql.append(" and DESCRIPTION like '%' ? '%'");
+                paramList.add(description);
+            }
+			PreparedStatement statement =conn.prepareStatement(sql.toString());
+			for(int i=0;i<paramList.size();i++)
+			{
+				statement.setString(i+1,paramList.get(i));
+			}
+			ResultSet rs=statement.executeQuery();
+			List<Message>messageList =new ArrayList<Message>();
+			while (rs.next())
+			{
+				Message message =new Message();
+				messageList.add(message);
+				message.setId(rs.getString("ID"));
+				message.setCommand(rs.getString("COMMAND"));
+				message.setDescription(rs.getString("DESCRIPTION"));
+				message.setContent(rs.getString("CONTENT"));
+			}
+			req.setAttribute("messageList", messageList);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
