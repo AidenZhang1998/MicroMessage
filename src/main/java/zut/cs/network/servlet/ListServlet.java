17363c586c9b047641bb480edb47baf4091e166e
LetMe666 <201608030215@zut.edu.cn>
2019-01-24 20:18:17 +0800

    Upgrade architecture：bean-dao-service-servlet

--- src/main/java/zut/cs/network/servlet/ListServlet.java
+++ src/main/java/zut/cs/network/servlet/ListServlet.java
@@ -1,4 +1,5 @@
 package zut.cs.network.servlet;
+
 import java.io.IOException;
 import java.sql.Connection;
 
@@ -10,65 +11,37 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import zut.cs.network.bean.Message;
+import zut.cs.network.service.ListService;
 
 /*
  * 列表初始化控制
  */
 @SuppressWarnings("serial")
-public class ListServlet extends HttpServlet{
+public class ListServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		try {
-		    req.setCharacterEncoding("UTF-8");
-			String command= req.getParameter("command");
-			String description =req.getParameter("description");
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://114.116.163.105:3306/micro_message?useUnicode=true&characterEncoding=utf-8","root","Aa184550!");
-			StringBuilder sql=new StringBuilder("select ID,COMMAND,DESCRIPTION,CONTENT FROM MESSAGE where 1=1");
-			List<String> paramList = new ArrayList<String>();
-			if (command!=null && !"".equals(command.trim())){
-                sql.append(" and COMMAND=?");
-                paramList.add(command);
-            }
-            if (description != null && !"".equals(description.trim())){
-                sql.append(" and DESCRIPTION like '%' ? '%'");
-                paramList.add(description);
-            }
-			PreparedStatement statement =conn.prepareStatement(sql.toString());
-			for(int i=0;i<paramList.size();i++)
-			{
-				statement.setString(i+1,paramList.get(i));
-			}
-			ResultSet rs=statement.executeQuery();
-			List<Message>messageList =new ArrayList<Message>();
-			while (rs.next())
-			{
-				Message message =new Message();
-				messageList.add(message);
-				message.setId(rs.getString("ID"));
-				message.setCommand(rs.getString("COMMAND"));
-				message.setDescription(rs.getString("DESCRIPTION"));
-				message.setContent(rs.getString("CONTENT"));
-			}
-			req.setAttribute("messageList", messageList);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// 设置编码
+		req.setCharacterEncoding("UTF-8");
+		// 接受页面传值
+		String command = req.getParameter("command");
+		String description = req.getParameter("description");
+		// 向页面传值
+		req.setAttribute("command", command);
+		req.setAttribute("description", description);		
+		ListService listService = new ListService();
+		//查询消息列表并传给页面
+		req.setAttribute("messageList", listService.QueryMessageList(command, description));
+		// 跳转页面
 		req.getRequestDispatcher("/WEB-INF/jsp/back/index.jsp").forward(req, resp);
-		//跳转
+
 	}
 
 	@Override
@@ -76,5 +49,5 @@ public class ListServlet extends HttpServlet{
 		// TODO Auto-generated method stub
 		this.doGet(req, resp);
 	}
-   
+
 }
