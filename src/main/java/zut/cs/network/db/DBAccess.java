1386cb07e999cca12074c053f9929d8682cd8ae9
LetMe666 <201608030215@zut.edu.cn>
2019-01-25 22:42:10 +0800

    Introduce Mybatis and   Complete data display

---
+++ src/main/java/zut/cs/network/db/DBAccess.java
@@ -0,0 +1,28 @@
+package zut.cs.network.db;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+/**
+ * 访问数据库类
+ *
+ */
+public class DBAccess {
+ public SqlSession getSqlSession() throws IOException
+ {
+	 
+	Reader reader=Resources.getResourceAsReader("/zut/cs/network/config/Configuration.xml");
+	SqlSessionFactory sqlSessionFactory = new  SqlSessionFactoryBuilder().build(reader);
+	SqlSession sqlSession =sqlSessionFactory.openSession();	
+		
+		
+	return sqlSession;
+	 
+ }
+}
--- src/main/webapp/WEB-INF/jsp/back/index.jsp
+++ src/main/webapp/WEB-INF/jsp/back/index.jsp
@@ -1,6 +1,6 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%
+<% 
     String path = request.getContextPath();
 	String basePath = request.getScheme() + "://"+request.getServerName()+":"+request.getServerPort()+path+"/";
 %>
